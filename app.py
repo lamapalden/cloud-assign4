b6737b55f5fedcbedb351a3c01d685334b5ed314
Palden Lama <lamapalden@gmail.com>
2021-11-18 19:27:30 -0600

    created a simple web app (app.py)

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Congratulations, it's a web app!"
+
+@app.route("/<int:celsius>")
+def fahrenheit_from(celsius):
+    print ("Converting celcius to fahrenheit", flush=True)
+    """Convert Celsius to Fahrenheit degrees."""
+    fahrenheit = float(celsius) * 9 / 5 + 32
+    fahrenheit = round(fahrenheit, 3)  # Round to three decimal places
+    return str(fahrenheit)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
